5547a7a83b8a63d5bf71efa64fdf21b5be564950
Mahdi <Mahdi_Najafzadeh_1383@outlook.com>
2022-10-01 13:48:11 +0330

    Add Try Catch System

--- middleware/checkTeamNameLabel.js
+++ middleware/checkTeamNameLabel.js
@@ -4,60 +4,66 @@ const { activeDirectory } = require("../Services/ActiveDirctory/index");
 const putAPI = require("../Services/request/put");
 
 module.exports = async (req, res, next) => {
-  // log
-  console.log("check Label MW Running ...");
-  // variables importing
-  const usernameInRequest = req.body.user.name;
-  const projectID = req.body.object_attributes.project_id;
-  const issueID = req.body.object_attributes.iid;
-  let userTeamName = "";
-  // check request parameters
-  if (usernameInRequest) {
-    userTeamName = activeDirectory.getDataUser(usernameInRequest).teamName;
-  }
-  // log Data for Debugging
-  // console.log(`------------------------------------------------------`);
-  // console.log(`User Name      : ${usernameInRequest}`);
-  // console.log(`Uesr Team Name : ${userTeamName}`);
-  // console.log(`Project ID     : ${projectID}`);
-  // console.log(`Issue ID       : ${issueID}`);
-  // console.log(`Labels         : ${req.body.object_attributes.labels}`);
-  // console.log(`------------------------------------------------------`);
+  try {
+    // log
+    console.log("check Label MW Running ...");
+    // variables importing
+    const usernameInRequest = req.body.user.name;
+    const projectID = req.body.object_attributes.project_id;
+    const issueID = req.body.object_attributes.iid;
+    let userTeamName = "";
+    // check request parameters
+    if (usernameInRequest) {
+      userTeamName = activeDirectory.getDataUser(usernameInRequest).teamName;
+    }
+    // log Data for Debugging
+    // console.log(`------------------------------------------------------`);
+    // console.log(`User Name      : ${usernameInRequest}`);
+    // console.log(`Uesr Team Name : ${userTeamName}`);
+    // console.log(`Project ID     : ${projectID}`);
+    // console.log(`Issue ID       : ${issueID}`);
+    // console.log(`Labels         : ${req.body.object_attributes.labels}`);
+    // console.log(`------------------------------------------------------`);
 
-  // import All variable
-  let checkLabelTeamName = true;
-  let allLabelOnIssue = [];
-  let allLabelOnIssueString = "";
-  // check all label
-  for (const label of req.body.object_attributes.labels) {
-    if (label.title === userTeamName) {
-      checkLabelTeamName = true;
-      break;
-    } else {
-      checkLabelTeamName = false;
-    }
-    // add all label
-    allLabelOnIssue.push(label.title);
-  }
-  // check to Do PUT or Not !
-  if (!checkLabelTeamName) {
-    // Add User Team Name to All Labels
-    allLabelOnIssue.push(userTeamName);
-    // Ready Label to PUT API --> Array to String
-    for (let index = 0; index < allLabelOnIssue.length; index++) {
-      if (index === allLabelOnIssue.length - 1) {
-        // for last label
-        allLabelOnIssueString += allLabelOnIssue[index];
+    // import All variable
+    let checkLabelTeamName = true;
+    let allLabelOnIssue = [];
+    let allLabelOnIssueString = "";
+    // check all label
+    for (const label of req.body.object_attributes.labels) {
+      if (label.title === userTeamName) {
+        checkLabelTeamName = true;
+        break;
       } else {
-        // for another labels
-        allLabelOnIssueString += allLabelOnIssue[index] + ",";
+        checkLabelTeamName = false;
       }
+      // add all label
+      allLabelOnIssue.push(label.title);
     }
-    console.log(
-      // Modify Labels --> PUT API
-      await putAPI(`projects/${projectID}/issues/${issueID}/`, {
-        labels: allLabelOnIssueString,
-      })
-    );
+    // check to Do PUT or Not !
+    if (!checkLabelTeamName) {
+      // Add User Team Name to All Labels
+      allLabelOnIssue.push(userTeamName);
+      // Ready Label to PUT API --> Array to String
+      for (let index = 0; index < allLabelOnIssue.length; index++) {
+        if (index === allLabelOnIssue.length - 1) {
+          // for last label
+          allLabelOnIssueString += allLabelOnIssue[index];
+        } else {
+          // for another labels
+          allLabelOnIssueString += allLabelOnIssue[index] + ",";
+        }
+      }
+      console.log(
+        // Modify Labels --> PUT API
+        await putAPI(`projects/${projectID}/issues/${issueID}/`, {
+          labels: allLabelOnIssueString,
+        })
+      );
+    }
+  } catch (error) {
+    console.log("Error: " + error);
+  } finally {
+    next();
   }
 };
--- middleware/removeLabel.js
+++ middleware/removeLabel.js
@@ -2,20 +2,26 @@ const GetAPI = require("../Services/request/get");
 const DeleteAPI = require("../Services/request/delete");
 
 module.exports = async () => {
-  // log
-  console.log("RUN Remvoe Labels!");
-  // get All labels data
-  const labels = await GetAPI("projects/183/labels");
-  // console.log("Next API");
-  // console.log(labels);
-  // console.log("-----------------------------------------");
-  for (label of labels.data) {
-    console.log(`projects/183/labels/${label.id}`);
-    const resAPI = await DeleteAPI(`projects/183/labels/${label.id}`);
-    if (resAPI.state) {
-      console.log(`Label ${label.name} --> Delete!`);
-    } else {
-      console.log(`Label ${label.name} --> Not Delete!`);
+  try {
+    // log
+    console.log("RUN Remvoe Labels!");
+    // get All labels data
+    const labels = await GetAPI("projects/183/labels");
+    // Delete all labels
+    for (label of labels.data) {
+      // RUN Delete API
+      const resAPI = await DeleteAPI(`projects/183/labels/${label.id}`);
+      // Check status API
+      if (resAPI.state) {
+        console.log(`Label ${label.name} --> Delete!`);
+      } else {
+        console.log(`Label ${label.name} --> Not Delete!`);
+      }
     }
+  } catch (error) {
+    // log Error
+    console.log("Error: " + error);
+  } finally {
+    next();
   }
 };
