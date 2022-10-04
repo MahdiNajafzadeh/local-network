d0ceb2509c62348df7595585599d0c8249d61f0f
“[Mahdi <Mahdi_Najafzadeh_1383@outlook.com>
2022-10-04 17:27:56 +0330

    update settting

--- testAl.js
+++ testAl.js
@@ -1,7 +1,11 @@
-let me;
-if(me)
-{
-  console.log("UN ==> true");
-}else{
-  console.log("UN ==> false");
+async function Time() {
+  while (true) {
+    const date = new Date();
+    await setTimeout(
+    () => console.log( date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()), 1000);
+    console.clear();
+  }
 }
+
+Time();
+console.log("Time");
