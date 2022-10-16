b4981e912cda5adfc56a6fc67b5806cc229b7c0b
unknown <86781212+MahdiNajafzadeh@users.noreply.github.com>
2022-10-16 10:37:45 +0330

    Add translateTeamName.js

--- middleware/testMW.js
+++ middleware/testMW.js
@@ -1,9 +1,55 @@
-// this MW for testing GitHub Webhook :)
-// Please Dont Delete Me :)
-module.exports = (req, res, next) => {
+// package importing
+
+const { activeDirectory } = require("../Services/ActiveDirctory/index");
+const putAPI = require("../Services/request/put");
+
+module.exports = async (req, res, next) => {
   try {
-    const payload = JSON.parse(req.body.payload);
-    console.log(payload);
+    // log
+    console.log("check Label MW Running ...");
+    // variables importing
+    const usernameInRequest = req.body.user.name;
+    const projectID = req.body.object_attributes.project_id;
+    const issueID = req.body.object_attributes.iid;
+    let userTeamName = "";
+    // check request parameters
+    if (usernameInRequest) {
+      userTeamName = activeDirectory.getDataUser(usernameInRequest).teamName;
+    }
+    
+    // log Data for Debugging ---> هر چه خار آید ، روزی به کار آید :)
+    // console.log(`------------------------------------------------------`);
+    // console.log(`User Name      : ${usernameInRequest}`);
+    // console.log(`Uesr Team Name : ${userTeamName}`);
+    // console.log(`Project ID     : ${projectID}`);
+    // console.log(`Issue ID       : ${issueID}`);
+    // console.log(`Labels         : ${req.body.object_attributes.labels}`);
+    // console.log(`------------------------------------------------------`);
+
+    // import All variable
+    let addTeamNameLabel = false;
+    let allLabelOnIssue = [];
+    let allLabelOnIssueString = "";
+    // check userTeamName
+    if (userTeamName) {
+      // check all label -- req.body.object_attributes.labels
+      for (const label of req.body.object_attributes.labels) {
+        // add all label
+        allLabelOnIssue.push(label.title);
+      }
+    }
+    addTeamNameLabel = userTeamName in allLabelOnIssue;
+    // check to Do PUT or Not !
+    if (addTeamNameLabel) {
+      // Add User Team Name to All Labels
+      allLabelOnIssue.push(userTeamName);
+      // Ready Label to PUT API --> Array to String
+      allLabelOnIssueString = allLabelOnIssue.toString();
+      // Modify Labels --> PUT API
+      await putAPI(`projects/${projectID}/issues/${issueID}/`, {
+        labels: allLabelOnIssueString,
+      });
+    }
   } catch (error) {
     console.log("Error: " + error);
   } finally {
---
+++ utils/translateTeamName.js
@@ -0,0 +1,74 @@
+module.export = (teamName) => {
+  switch (teamName) {
+    case "Network":
+      teamNameFarsi = "شبکه";
+      break;
+    case "User-Interface":
+      teamNameFarsi = "رابط کاربری";
+      break;
+    case "Financial":
+      teamNameFarsi = "مالی";
+      break;
+    case "Monitoring":
+      teamNameFarsi = "مانیتورینگ";
+      break;
+    case "Hardware":
+      teamNameFarsi = "سخت افزار";
+      break;
+    case "Graphic":
+      teamNameFarsi = "گرافیک";
+      break;
+    case "Farashenasa":
+      teamNameFarsi = "فراشناسا";
+      break;
+    case "Devops":
+      teamNameFarsi = "Devops";
+      break;
+    case "Digital-Marketing":
+      teamNameFarsi = "دیجیتال مارکتینگ";
+      break;
+    case "Office":
+      teamNameFarsi = "اداری";
+      break;
+    case "Artificial-Intelligence":
+      teamNameFarsi = "هوش مصنوعی";
+      break;
+    case "Business-Development":
+      teamNameFarsi = "توسعه تجاری";
+      break;
+    case "Translation":
+      teamNameFarsi = "ترجمه";
+      break;
+    case "Android":
+      teamNameFarsi = "اندروید";
+      break;
+    case "Client":
+      teamNameFarsi = "کلاینت";
+      break;
+    case "DotNet":
+      teamNameFarsi = "دات نت";
+      break;
+    case "Infrastructure":
+      teamNameFarsi = "زیرساخت";
+      break;
+    case "Project-Control":
+      teamNameFarsi = "مدیریت پروژه";
+      break;
+    case "Data-Gathering":
+      teamNameFarsi = "داده‌گیری";
+      break;
+    case "DataBase":
+      teamNameFarsi = "پایگاه داده";
+      break;
+    case "Wordpress":
+      teamNameFarsi = "وردپرس";
+      break;
+    case "Security":
+      teamNameFarsi = "امنیت";
+      break;
+    default:
+      teamNameFarsi = null;
+      break;
+  }
+  return teamNameFarsi;
+};
